6259584bdd3ce1d4abfe5c4df7266f77cab81751
DESKTOP-GFHF2H5\gong6 <gong6697@naver.com>
2020-11-13 17:12:16 +0900

    alarm test

---
+++ routes/healthPill.js
@@ -0,0 +1,33 @@
+const schedule = require("node-schedule");
+const express = require("express");
+// 미들웨어가 꼭 필요한가? - 설정해둔 약이 있는지 확인?
+const { Control, Date } = require("../models");
+const router = express.Router();
+const moment = require("moment");
+const rule = new schedule.RecurrenceRule();
+
+router.post("/api/main/health", async (req, res) => {
+    const {
+        controlStart,
+        controlEnd,
+        controlHour,
+        controlMinute,
+    } = req.body;
+    try {
+        const excontrol = await Control.findAll({});
+        rule.hour = controlHour;
+        rule.minute = controlMinute;
+        const j = schedule.scheduleJob(rule, function () {
+            alert('성공!')
+        });
+        return res.status(201).json({ completed: true });
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+router.get("/api/main/health", async (req, res) => {
+
+});
+
+module.exports = router;
