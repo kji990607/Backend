8c9902d5563709b32ba51b1c8c6dba02c419cde9
Jeongin <kji990607@gmail.com>
2020-11-14 10:53:47 +0900

    Fix router error

--- routes/healthPill.js
+++ routes/healthPill.js
@@ -7,27 +7,24 @@ const moment = require("moment");
 const rule = new schedule.RecurrenceRule();
 
 router.post("/api/main/health", async (req, res) => {
-    const {
-        controlStart,
-        controlEnd,
-        controlHour,
-        controlMinute,
-    } = req.body;
-    try {
-        const excontrol = await Control.findAll({});
-        rule.hour = controlHour;
-        rule.minute = controlMinute;
-        const j = schedule.scheduleJob(rule, function () {
-            alert('성공!')
-        });
-        return res.status(201).json({ completed: true });
-    } catch (error) {
-        console.error(error);
-        return next(error);
-    }
+  const { controlStart, controlEnd, controlHour, controlMinute } = req.body;
+  try {
+    //     const excontrol = await Control.findAll({});
+    //     rule.hour = controlHour;
+    //     rule.minute = controlMinute;
+    //     const j = schedule.scheduleJob(rule, function () {
+    //         alert('성공!')
+    //     });
+    //     return res.status(201).json({ completed: true });
+    res.send("라우터 연결 됨");
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
-router.get("/api/main/health", async (req, res) => {
 
-});
+// router.get("/api/main/health", async (req, res) => {
+//
+// });
 
 module.exports = router;
