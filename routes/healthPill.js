4c091609c546b7fe8b43a311881f46cd224d3c59
DESKTOP-GFHF2H5\gong6 <gong6697@naver.com>
2020-11-16 14:07:51 +0900

    edit push

--- routes/healthPill.js
+++ routes/healthPill.js
@@ -1,7 +1,7 @@
+const { Control, Date } = require("../models");
 const schedule = require("node-schedule");
 const express = require("express");
 const { isLoggedIn } = require("./middlewares");
-const { Control, Date } = require("../models");
 const cron = require("node-cron");
 const router = express.Router();
 const moment = require("moment");
@@ -11,43 +11,35 @@ const j = schedule.scheduleJob(rule, function () {});
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 
-    router.post("/api/main/control", async (req, res) => {
-      const {controlStart, controlEnd, controlHour, controlMinute} = req.body;
-      try {
-        const {
-          controlStart, //controlStart 정하고 controlEnd가 되면 취소돼야함. controlEnd+1=j.cancel
-          controlEnd,
-          controlHour,
-        } = req.body;
-        try {
-          const Control = await Control.findAll({});
-          const controlTime = controlHour.split(':');
-          rule.hour = controlTime[0];
-          rule.minute = controlTime[1];
-          const j = schedule.scheduleJob(rule, function () {
-            alert('성공!') // alert 말고
-          });
-          return res.status(201).json({completed: true});
-          return res.send("라우터 연결 됨");
-        } catch (error) {
-          console.error(error);
-          return next(error);
-        }
-      } catch (error) {
-        console.error(error);
-        return next(error);
-      }
+router.post("/api/main/control", async (req, res) => {
+  const {controlStart, controlEnd, controlHour, controlMinute} = req.body;
+  try {
+    //const Control = await Control.findAll({});
+    await Control.create({
+
+      controlStart: controlStart,
+      controlEnd: controlEnd,
+      controlHour: controlHour,
+      userId: req.user.id,
     });
 
-/*
-const alarm = cron.schedule('0 controlTime[1] controlTime[0] *%/24 * *', () => {
-    console.log('알람 울리기');
-    alert('성공');
+    const controlTime = controlHour.split(':');
+    rule.hour = controlTime[0];
+    rule.minute = controlTime[1];
+    const j = schedule.scheduleJob(rule, function () {
+      alert('성공!') // alert 말고
+    });
+    return res.status(201).json({completed: true});
+    return res.send("라우터 연결 됨");
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
-If(date === controlStart){
-    alarm.start();
-    } else if (date === controlEnd){
-        alarm.destroy(); }
+
+/*
+
+
 
 If (controlEnd === null){
     set controlEnd = controlStart+180
@@ -55,33 +47,38 @@ If (controlEnd === null){
         controlEnd: controlEnd}
 const i = new Date();
 for(i=controlStart; i<= controlEnd; i++){
-    i가 start 이상 end 이하일 때 계속 울리기
-
     const alarm = cron.schedule('0 controlTime[1] controlTime[0] *%/24 * *', () => {
     console.log('알람 울리기');
     alert('성공');
-}
+    If (i>controlEnd){
+      alarm.destroy(); }
+});
+
 
  */
 
+
+
 router.get("/api/main/control", isLoggedIn, async (req, res) => {
-  //날짜를 어떻게 받아올건지? 주소로?? 아님 req.body로?
-  //null일 경우에 현재는 에러. 이 상황 어떻게 할지는 추후 논의
-  const date = req.body.date;
+  const control = req.body.control;
   try {
-    const exDate = await Date.findOne({
-      attributes: ["isControl"],
-      where: { date: date, userId: req.user.id },
+    Date.findOne({
+      attributes:[isControl],
+      where : {date:date, userId: req.user.id}, // DB의 Date테이블에서 iscontrol 값 받아와서 True인지 False인지 확인
     });
-    if (exDate.isControl) {
-      res.send("오늘 피임약 복용 완료");
+    if (isControl) {
+      res.send("오늘 약 복용 완료");
     } else {
-      res.send("오늘 피임약 복용 전");
+      res.send("오늘 약 복용 전");
     }
   } catch (error) {
     console.error(error);
     return next(error);
   }
 });
+
+// router.get("/api/main/control", async (req, res) => {
+//
+// });
 
 module.exports = router;
