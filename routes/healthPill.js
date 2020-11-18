a5c731676a6c6d30f57ebdc4b65e766badf26d40
Jeongin <kji990607@gmail.com>
2020-11-18 16:34:45 +0900

    Fix error with default controlEnd

--- routes/healthPill.js
+++ routes/healthPill.js
@@ -1,4 +1,4 @@
-const { Control, Date} = require("../models");
+const { Control, Date } = require("../models");
 const schedule = require("node-schedule");
 const express = require("express");
 const { isLoggedIn } = require("./middlewares");
@@ -8,60 +8,73 @@ const moment = require("moment");
 const rule = new schedule.RecurrenceRule();
 //const j = schedule.scheduleJob(rule, function () {});
 
-require('moment-timezone');
+require("moment-timezone");
 moment.tz.setDefault("Asia/Seoul");
 
 router.post("/api/main/control", isLoggedIn, async (req, res) => {
-  const {controlStart, controlEnd, controlTime} = req.body;
-  console.log("뀨유 로그인 사용자",req.user.id);
+  const { controlStart, controlEnd, controlTime } = req.body;
+  console.log("뀨유 로그인 사용자", req.user.id);
   try {
-    const controlClock = controlTime.split(':');
+    const controlClock = controlTime.split(":");
     const controlHour = controlClock[0];
     const controlMinute = controlClock[1];
-    if(controlEnd === null) {
+    if (!controlEnd) {
+      // controlEnd가 NULL이면 180일 뒤로 자동지정
       await Control.create({
         controlStart: controlStart,
-        controlEnd: controlStart+180, // controlEnd 안정하면 180일 뒤로 자동지정
+        controlEnd: moment(controlStart, "YYYY-MM-DD")
+          .add(180, "d")
+          .format("YYYY-MM-DD"),
         controlHour: controlHour,
         controlMinute: controlMinute,
         userId: req.user.id,
-      },{
-        where: {userId: req.user.id},
       });
     } else {
-      await Control.create({
-        controlStart: controlStart,
-        controlEnd: controlEnd,
-        controlHour: controlHour,
-        controlMinute: controlMinute,
-        userId: req.user.id,
-      },{
-        where: {userId: req.user.id},
-      });
+      await Control.create(
+        {
+          controlStart: controlStart,
+          controlEnd: controlEnd,
+          controlHour: controlHour,
+          controlMinute: controlMinute,
+          userId: req.user.id,
+        },
+        {
+          where: { userId: req.user.id },
+        }
+      );
     }
     console.log("ocontrol.create까지 됨");
     console.log(req.user.id);
     const exControl = await Control.findAll({
-      limit:1,
-      attributes: ["controlStart", "controlEnd", "controlHour", "controlMinute"],
-      where : {userId: req.user.id},
-      order: [ [ 'createdAt', 'DESC' ]]
+      limit: 1,
+      attributes: [
+        "controlStart",
+        "controlEnd",
+        "controlHour",
+        "controlMinute",
+      ],
+      where: { userId: req.user.id },
+      order: [["createdAt", "DESC"]],
     });
     console.log("findAll까지");
     console.log(exControl.controlStart, exControl.controlEnd);
-    for (let i=exControl.controlStart; i<=exControl.controlEnd; i++) {
-      var alarm = schedule.schedulejob('0 controlMinute controlHour * * *', () => { //cron? schedule?
-        console.log('알람 울리기');
-        alert('성공'); //res.redirect로 알람 페이지로 연결
-        if (i > exControl.controlEnd) {
-          alarm.cancel();
+    for (let i = exControl.controlStart; i <= exControl.controlEnd; i++) {
+      var alarm = schedule.schedulejob(
+        "0 controlMinute controlHour * * *",
+        () => {
+          //cron? schedule?
+          console.log("알람 울리기");
+          alert("성공"); //res.redirect로 알람 페이지로 연결
+          if (i > exControl.controlEnd) {
+            alarm.cancel();
+          }
         }
-      });
+      );
     }
 
-    return res.status(201).json({completed: true});
+    return res.status(201).json({ completed: true });
     return res.send("라우터 연결 됨");
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     return error;
   }
@@ -74,8 +87,6 @@ rule.hour = controlTime[0];
       alert('성공!') // alert 말고
     });
  */
-
-
 
 router.get("/api/control", isLoggedIn, async (req, res) => {
   //날짜는 주소로 받아옴
