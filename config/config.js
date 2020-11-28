6f3e12d856f15c7994b4de9d8557b7d1da9a5063
wlsdk7245 <wlsdk7245@naver.com>
2020-11-28 18:20:57 +0900

    Revert "heroku-complete;"
    This reverts commit 7751d33b07f424759a1c4f36c060815bbb27ff47.

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: "admin",
+    username: "root",
     password: process.env.DB_SECRET, //.env파일 생성 후 'DB_SECRET=각자 mysql비밀번호'로 쓰기
     database: "Fullmoon",
-    host: "hy-fullmoon.c78lrhmw2ur6.ap-northeast-2.rds.amazonaws.com",
+    host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
