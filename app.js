d462c4f5918863a5840caabc358ed5c7eacaef69
Jeongin <kji990607@gmail.com>
2020-12-02 12:08:06 +0900

    서버 테스트

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use("/", healthPillRouter);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
