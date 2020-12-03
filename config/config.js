7080d9e1c2f2fd1044d33e20db2f173f7e366cf4
Jeongin <kji990607@gmail.com>
2020-12-03 21:05:36 +0900

    Update db name for production

--- config/config.js
+++ config/config.js
@@ -13,14 +13,14 @@ module.exports = {
   test: {
     username: "root",
     password: process.env.DB_SECRET,
-    database: "database_test",
+    database: "Fullmoon",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: "root",
     password: process.env.DB_SECRET,
-    database: "database_production",
+    database: "Fullmoon",
     host: "127.0.0.1",
     dialect: "mysql",
   },
