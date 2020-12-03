211dac266f1aeb1cc09e8c39091f2014e95b1d2e
Jeongin <kji990607@gmail.com>
2020-12-04 01:59:22 +0900

    Add checkUserID1

--- proxyControllers/realCycle.js
+++ proxyControllers/realCycle.js
@@ -2,31 +2,56 @@
 const { User } = require("../models");
 
 const json = require("./responseController");
+let userId = 0;
+let meanCycle = 0;
+
+const checkUserID1 = async (req, res, next) => {
+  const exUser = await User.findOne({
+    attributes: ["id"],
+    where: { id: req.body.parameters.user_ID },
+  });
+  try {
+    if (exUser) {
+      userId = exUser.id;
+      return;
+    } else {
+      userId = 0;
+    }
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
 
 //평균 주기 물어볼 경우
 const askMeanCycle = async (req, res, next) => {
   console.log("정상 작동");
   console.log("요청:", req.body);
-  // try {
-  //   //조회
-  //   const exUser = await User.findOne({
-  //     attributes: ["meanCycle"],
-  //     where: { userId: req.user.id },
-  //   });
-  //   const meanCycle = exUser.meanCycle;
-  //   //처리 후 responseController형식에 맞춰줘야 함. nugu 홈페이지 responseSample 형식으로
-  //   const resObj = json.resSample();
-  //   resObj.version = req.body.version;
-  //   resObj.output.meanCycle = meanCycle;
-  //   res.json(resObj);
-  //   res.end();
-  //   return;
-  // } catch (error) {
-  //   console.error(error);
-  //   return next(error);
-  // }
+  try {
+    //조회
+    const exUser = await User.findOne({
+      attributes: ["meanCycle"],
+      where: { id: userId },
+    });
+    if (exUser) {
+      meanCycle = exUser.meanCycle;
+    } else {
+      meanCycle = 0;
+    }
+    //처리 후 responseController형식에 맞춰줘야 함. nugu 홈페이지 responseSample 형식으로x
+    const resObj = json.resSample();
+    resObj.version = req.body.version;
+    resObj.output.meanCycle = meanCycle;
+    res.json(resObj);
+    res.end();
+    return;
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 };
 
 module.exports = {
+  checkUserID1,
   askMeanCycle,
 };
--- routes/proxyIndex.js
+++ routes/proxyIndex.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { isLoggedIn } = require("./middlewares");
 
 //proxyController 예시 형식
-const { askMeanCycle } = require("../proxyControllers/realCycle");
+const { checkUserID1, askMeanCycle } = require("../proxyControllers/realCycle");
 
 //프록시 주소 라우터
+router.post("/check.userID1", checkUserID1);
 router.post("/answer.meanCycle", askMeanCycle);
 
 module.exports = router;
