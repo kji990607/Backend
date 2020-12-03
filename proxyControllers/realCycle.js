a766dc24072b3414f2e3b214a851d7d71436e988
Jeongin <kji990607@gmail.com>
2020-12-04 02:20:17 +0900

    Fix error with checkUserID1

--- proxyControllers/realCycle.js
+++ proxyControllers/realCycle.js
@@ -2,41 +2,21 @@
 const { User } = require("../models");
 
 const json = require("./responseController");
-let userId = 0;
-let meanCycle = 0;
 
-const checkUserID1 = async (req, res, next) => {
-  const exUser = await User.findOne({
-    attributes: ["id"],
-    where: { id: req.body.action.parameters.user_ID.value},
-  });
-  try {
-    if (exUser) {
-      userId = exUser.id;
-      return;
-    } else {
-      userId = 0;
-    }
-  } catch (error) {
-    console.error(error);
-    return next(error);
-  }
-};
+let meanCycle = 0;
 
 //평균 주기 물어볼 경우
 const askMeanCycle = async (req, res, next) => {
-  console.log("정상 작동");
-  console.log("요청:", req.body);
   try {
     //조회
     const exUser = await User.findOne({
       attributes: ["meanCycle"],
-      where: { id: userId },
+      where: { id: req.body.action.parameters.user_ID },
     });
     if (exUser) {
       meanCycle = exUser.meanCycle;
     } else {
-      meanCycle = 0;
+      meanCycle = "없음";
     }
     //처리 후 responseController형식에 맞춰줘야 함. nugu 홈페이지 responseSample 형식으로x
     const resObj = json.resSample();
@@ -52,6 +32,5 @@ const askMeanCycle = async (req, res, next) => {
 };
 
 module.exports = {
-  checkUserID1,
   askMeanCycle,
 };
--- routes/proxyIndex.js
+++ routes/proxyIndex.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 //proxyController 예시 형식
-const { checkUserID1, askMeanCycle } = require("../proxyControllers/realCycle");
+const { askMeanCycle } = require("../proxyControllers/realCycle");
 
 //프록시 주소 라우터
-router.post("/check.userID1", checkUserID1);
 router.post("/answer.meanCycle", askMeanCycle);
 
 module.exports = router;
