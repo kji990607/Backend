e0dfd6c235cba52250cbd41c486a3eecdf1652ff
Jeongin <kji990607@gmail.com>
2020-12-07 04:59:25 +0900

    Add askRealCycle undefined case

--- proxyControllers/realCycle.js
+++ proxyControllers/realCycle.js
@@ -3,9 +3,10 @@ const { User, Cycle } = require("../models");
 
 const json = require("./responseController");
 
+const resObj = json.resSample();
 let meanCycle,
-  cycleStart,
-  cycleEnd = 0;
+    cycleStart,
+    cycleEnd = 0;
 
 //평균 주기 물어볼 경우
 const askMeanCycle = async (req, res, next) => {
@@ -25,7 +26,6 @@ const askMeanCycle = async (req, res, next) => {
       meanCycle = exUser.meanCycle;
     }
     //처리 후 responseController형식에 맞춰줘야 함. nugu 홈페이지 responseSample 형식으로
-    const resObj = json.resSample();
     resObj.version = req.body.version;
     resObj.output.meanCycle = meanCycle;
     res.json(resObj);
@@ -40,38 +40,64 @@ const askMeanCycle = async (req, res, next) => {
 //최근 주기 물어볼 경우
 const askRealCycle = async (req, res, next) => {
   try {
-    const recentCycle = await Cycle.findAll({
-      limit: 1,
-      attributes: ["bleedStart", "bleedEnd"],
-      where: { userId: req.body.action.parameters.user_ID.value },
-      order: [["bleedStart", "DESC"]],
-      raw: true,
+    const exUser = await User.findOne({
+      attributes: ["id"],
+      where: { id: req.body.action.parameters.user_ID.value },
     });
-    if (!recentCycle) {
+    if (!exUser) {
       //아이디 틀린 경우
       cycleStart = -1;
       cycleEnd = -1;
-    } else if (recentCycle[0].bleedStart === null) {
-      //정보가 없는 경우
-      cycleStart = -2;
-      cycleEnd = -2;
-    } else if (recentCycle[0].bleedEnd === null) {
-      //시작만 있고 끝은 없는 주기인 경우
-      cycleStart = recentCycle[0].bleedStart;
-      cycleEnd = -2;
-    } else if (recentCycle) {
-      cycleStart = recentCycle[0].bleedStart;
-      cycleEnd = recentCycle[0].bleedEnd;
+      resObj.version = req.body.version;
+      resObj.output.cycleStart = cycleStart;
+      resObj.output.cycleEnd = cycleEnd;
+      console.log(resObj);
+      res.json(resObj);
+      res.end();
+      return;
+    } else {
+      //아이디 틀리지 않은 경우
+      const recentCycle = await Cycle.findAll({
+        limit: 1,
+        attributes: ["bleedStart", "bleedEnd"],
+        where: { userId: req.body.action.parameters.user_ID.value },
+        order: [["bleedStart", "DESC"]],
+        raw: true,
+      });
+      if (recentCycle[0] === undefined) {
+        //정보가 없는 경우
+        cycleStart = -2;
+        cycleEnd = -2;
+        resObj.version = req.body.version;
+        resObj.output.cycleStart = cycleStart;
+        resObj.output.cycleEnd = cycleEnd;
+        console.log(resObj);
+        res.json(resObj);
+        res.end();
+        return;
+      } else if (recentCycle[0].bleedEnd === null) {
+        //시작만 있고 끝은 없는 주기인 경우
+        cycleStart = recentCycle[0].bleedStart;
+        cycleEnd = -2;
+        resObj.version = req.body.version;
+        resObj.output.cycleStart = cycleStart;
+        resObj.output.cycleEnd = cycleEnd;
+        console.log(resObj);
+        res.json(resObj);
+        res.end();
+        return;
+      } else if (recentCycle) {
+        cycleStart = recentCycle[0].bleedStart;
+        cycleEnd = recentCycle[0].bleedEnd;
+        resObj.version = req.body.version;
+        resObj.output.cycleStart = cycleStart;
+        resObj.output.cycleEnd = cycleEnd;
+        console.log(resObj);
+        res.json(resObj);
+        res.end();
+        return;
+      }
     }
-    //처리 후 responseController형식에 맞춰줘야 함. nugu 홈페이지 responseSample 형식으로
-    const resObj = json.resSample();
-    resObj.version = req.body.version;
-    resObj.output.cycleStart = cycleStart;
-    resObj.output.cycleEnd = cycleEnd;
-    console.log(resObj);
-    res.json(resObj);
-    res.end();
-    return;
   } catch (error) {
     console.error(error);
     return next(error);
