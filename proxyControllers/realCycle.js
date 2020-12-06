7e915a32e6b6ee8fd4bdae2088efe05d625989a6
Jeongin <kji990607@gmail.com>
2020-12-07 03:46:42 +0900

    Add proxy askRealCycle

--- proxyControllers/realCycle.js
+++ proxyControllers/realCycle.js
@@ -3,7 +3,9 @@ const { User } = require("../models");
 
 const json = require("./responseController");
 
-let meanCycle = 0;
+let meanCycle,
+  cycleStart,
+  cycleEnd = 0;
 
 //평균 주기 물어볼 경우
 const askMeanCycle = async (req, res, next) => {
@@ -14,8 +16,10 @@ const askMeanCycle = async (req, res, next) => {
       where: { id: req.body.action.parameters.user_ID.value },
     });
     if (!exUser) {
+      //아이디가 틀린 경우
       meanCycle = "-1";
     } else if (exUser.meanCycle === null) {
+      //정보가 없는 경우
       meanCycle = "-2";
     } else if (exUser) {
       meanCycle = exUser.meanCycle;
@@ -33,6 +37,48 @@ const askMeanCycle = async (req, res, next) => {
   }
 };
 
+//최근 주기 물어볼 경우
+const askRealCycle = async (req, res, next) => {
+  try {
+    const recentCycle = await Cycle.findAll({
+      limit: 1,
+      attributes: ["bleedStart", "bleedEnd"],
+      where: { userId: req.body.action.parameters.user_ID.value },
+      order: [["bleedStart", "DESC"]],
+      raw: true,
+    });
+    if (!recentCycle) {
+      //아이디 틀린 경우
+      cycleStart = -1;
+      cycleEnd = -1;
+    } else if (recentCycle[0].bleedStart === null) {
+      //정보가 없는 경우
+      cycleStart = -2;
+      cycleEnd = -2;
+    } else if (recentCycle[0].bleedEnd === null) {
+      //시작만 있고 끝은 없는 주기인 경우
+      cycleStart = recentCycle[0].bleedStart;
+      cycleEnd = -2;
+    } else if (recentCycle) {
+      cycleStart = recentCycle[0].bleedStart;
+      cycleEnd = recentCycle[0].bleedEnd;
+    }
+    //처리 후 responseController형식에 맞춰줘야 함. nugu 홈페이지 responseSample 형식으로
+    const resObj = json.resSample();
+    resObj.version = req.body.version;
+    resObj.output.cycleStart = cycleStart;
+    resObj.output.cycleEnd = cycleEnd;
+    console.log(resObj);
+    res.json(resObj);
+    res.end();
+    return;
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
 module.exports = {
   askMeanCycle,
+  askRealCycle,
 };
--- routes/proxyIndex.js
+++ routes/proxyIndex.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 //proxyController 예시 형식
-const { askMeanCycle } = require("../proxyControllers/realCycle");
+const { askMeanCycle, askRealCycle } = require("../proxyControllers/realCycle");
+//const { askPillPredict } = require("../proxyControllers/pillPredict");
 
 //프록시 주소 라우터
 router.post("/answer.meanCycle", askMeanCycle);
+//router.post("/answer.pill", askPillPredict);
+router.post("/answer.cycle", askRealCycle);
 
 module.exports = router;
