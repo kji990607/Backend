f7ff3c85095706f3594d45374533238ee8590cd9
sihyunJung <gong6697@naver.com>
2020-12-08 21:16:21 +0900

    input test

--- proxyControllers/realCycle.js
+++ proxyControllers/realCycle.js
@@ -1,12 +1,14 @@
 //실제 생리 주기 관련 정보 처리
 const { User, Cycle } = require("../models");
+const Op = Sequelize.Op;
 
 const json = require("./responseController");
 
 const resObj = json.resSample();
 let meanCycle,
-    cycleStart,
-    cycleEnd = 0;
+  cycleStart,
+  cycleEnd,
+  outputCycle = 0;
 
 //평균 주기 물어볼 경우
 const askMeanCycle = async (req, res, next) => {
@@ -90,7 +92,135 @@ const askRealCycle = async (req, res, next) => {
   }
 };
 
+
+
+
+
+
+const answerRealStart = async (req, res, next) => {
+  console.log("요청 확인", req.body);
+//   console.log(req.body.action.parameters);
+//   // 생리주기 입력 정보를 유저가 확인하지 않았을 경우
+//   try {
+//     const exUser = await User.findOne({
+//       attributes: ["id"],
+//       where: { id: req.body.action.parameters.user_ID.value },
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return next(error);
+//   }
+//   if (!exUser) {
+//     //아이디 틀린 경우
+//     outputCycle = "아이디";
+//     resObj.version = req.body.version;
+//     resObj.output.outputCycle = outputCycle;
+//     console.log(resObj);
+//     res.json(resObj);
+//     res.end();
+//     return;
+//   } else {
+//     //아이디 틀리지 않은 경우
+//     const nowStart = await Cycle.findAll({
+//       attributes: ["bleedStart"],
+//       where: {
+//         [Op.and]: [
+//           { bleedStart: { [Op.gte]: find_start } },
+//           { bleedStart: { [Op.lte]: find_end } },
+//           { userId: req.user.id },
+//
+//
+//
+//     const nowStart = await Cycle.findAll({
+//       limit: 1,
+//       attributes: ["bleedStart"],
+//       where: { userId: req.body.action.parameters.user_ID.value },
+//       order: [["createdAt", "DESC"]],
+//       raw: true,
+//     });
+//     if (nowStart[0] === undefined) {
+//       await Cycle.create(
+//         {
+//           bleedStart: nowStart[0],
+//           userId: req.user.id,
+//         },
+//         {
+//           where: { userId: req.user.id },
+//         }
+//       );
+//     } else {
+//       await Cycle.update(
+//         {
+//           bleedStart: nowCycle[0],
+//           userId: req.user.id,
+//         },
+//         {
+//           where: { userId: req.user.id },
+//         }
+//       );
+//     }
+//   }
+// };
+//
+// const answerRealEnd = async (req, res, next) => {
+//   console.log(req.body);
+//   console.log(req.body.action.parameters);
+//   // 생리주기 입력 정보를 유저가 확인하지 않았을 경우
+//   try {
+//     const exUser = await User.findOne({
+//       attributes: ["id"],
+//       where: { id: req.body.action.parameters.user_ID.value },
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return next(error);
+//   }
+//   if (!exUser) {
+//     //아이디 틀린 경우
+//     outputCycle = "아이디";
+//     resObj.version = req.body.version;
+//     resObj.output.outputCycle = outputCycle;
+//     console.log(resObj);
+//     res.json(resObj);
+//     res.end();
+//     return;
+//   } else {
+//     //아이디 틀리지 않은 경우
+//     const nowEnd = await Cycle.findOne({
+//       limit: 1,
+//       attributes: ["bleedEnd"],
+//       where: { userId: req.body.action.parameters.user_ID.value },
+//       order: [["bleedEnd", "DESC"]],
+//       raw: true,
+//     });
+//     if (nowEnd[0] === undefined) {
+//       await Cycle.create(
+//         {
+//           bleedEnd: nowEnd[0],
+//           userId: req.user.id,
+//         },
+//         {
+//           where: { userId: req.user.id },
+//         }
+//       );
+//     } else {
+//       await Cycle.update(
+//         {
+//           bleedEnd: nowEnd[0],
+//           userId: req.user.id,
+//         },
+//         {
+//           where: { userId: req.user.id },
+//         }
+//       );
+//     }
+//   }
+// };
+};
+
 module.exports = {
   askMeanCycle,
   askRealCycle,
+  // answerRealEnd,
+  answerRealStart,
 };
--- routes/proxyIndex.js
+++ routes/proxyIndex.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { askMeanCycle, askRealCycle } = require("../proxyControllers/realCycle");
+const { askMeanCycle, askRealCycle, answerRealStart, answerRealEnd } = require("../proxyControllers/realCycle");
 const { askPillPredict } = require("../proxyControllers/pillPredict");
+
 
 //프록시 주소 라우터
 router.post("/answer.meanCycle", askMeanCycle);
 router.post("/answer.cycle", askRealCycle);
+router.post("/output.cycleStart", answerRealStart);
+router.post("/output.cycleEnd", answerRealEnd);
 
 router.post("/answer.pill", askPillPredict);
 
