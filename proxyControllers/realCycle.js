18346624ccbcaa21d62cc837e3a9982fbddc4229
sihyunJung <gong6697@naver.com>
2020-12-08 22:30:30 +0900

    date format check

--- proxyControllers/realCycle.js
+++ proxyControllers/realCycle.js
@@ -1,7 +1,7 @@
 //실제 생리 주기 관련 정보 처리
-const { User, Cycle } = require("../models");
-//const Sequelize = require("sequelize");
-//const Op = Sequelize.Op;
+const { User, Cycle} = require("../models");
+// const Sequelize = require("sequelize");
+// const Op = Sequelize.Op;
 
 const json = require("./responseController");
 
@@ -94,130 +94,160 @@ const askRealCycle = async (req, res, next) => {
 };
 
 
-
-
-
+/*
+const date_input = req.body.action.parameters.DATE.value;
+const date_split = date_input.split("-");
+const date_year = date_split[0];
+const date_month = date_split[1];
+const fin_start = moment(
+    date_year + "-" + date_month + "-01"
+).format("YYYY-MM-DD");
+const fin_end = req.body.action.parameters.DATE.value;
+*/
 
 const answerRealStart = async (req, res, next) => {
-  console.log("요청 확인", req.body);
-//   console.log(req.body.action.parameters);
-//   // 생리주기 입력 정보를 유저가 확인하지 않았을 경우
-//   try {
-//     const exUser = await User.findOne({
-//       attributes: ["id"],
-//       where: { id: req.body.action.parameters.user_ID.value },
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return next(error);
-//   }
-//   if (!exUser) {
-//     //아이디 틀린 경우
-//     outputCycle = "아이디";
-//     resObj.version = req.body.version;
-//     resObj.output.outputCycle = outputCycle;
-//     console.log(resObj);
-//     res.json(resObj);
-//     res.end();
-//     return;
-//   } else {
-//     //아이디 틀리지 않은 경우
-//     const nowStart = await Cycle.findAll({
-//       attributes: ["bleedStart"],
-//       where: {
-//         [Op.and]: [
-//           { bleedStart: { [Op.gte]: find_start } },
-//           { bleedStart: { [Op.lte]: find_end } },
-//           { userId: req.user.id },
-//
-//
-//
-//     const nowStart = await Cycle.findAll({
-//       limit: 1,
-//       attributes: ["bleedStart"],
-//       where: { userId: req.body.action.parameters.user_ID.value },
-//       order: [["createdAt", "DESC"]],
-//       raw: true,
-//     });
-//     if (nowStart[0] === undefined) {
-//       await Cycle.create(
-//         {
-//           bleedStart: nowStart[0],
-//           userId: req.user.id,
-//         },
-//         {
-//           where: { userId: req.user.id },
-//         }
-//       );
-//     } else {
-//       await Cycle.update(
-//         {
-//           bleedStart: nowCycle[0],
-//           userId: req.user.id,
-//         },
-//         {
-//           where: { userId: req.user.id },
-//         }
-//       );
-//     }
-//   }
-// };
-//
-// const answerRealEnd = async (req, res, next) => {
-//   console.log(req.body);
-//   console.log(req.body.action.parameters);
-//   // 생리주기 입력 정보를 유저가 확인하지 않았을 경우
-//   try {
-//     const exUser = await User.findOne({
-//       attributes: ["id"],
-//       where: { id: req.body.action.parameters.user_ID.value },
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return next(error);
-//   }
-//   if (!exUser) {
-//     //아이디 틀린 경우
-//     outputCycle = "아이디";
-//     resObj.version = req.body.version;
-//     resObj.output.outputCycle = outputCycle;
-//     console.log(resObj);
-//     res.json(resObj);
-//     res.end();
-//     return;
-//   } else {
-//     //아이디 틀리지 않은 경우
-//     const nowEnd = await Cycle.findOne({
-//       limit: 1,
-//       attributes: ["bleedEnd"],
-//       where: { userId: req.body.action.parameters.user_ID.value },
-//       order: [["bleedEnd", "DESC"]],
-//       raw: true,
-//     });
-//     if (nowEnd[0] === undefined) {
-//       await Cycle.create(
-//         {
-//           bleedEnd: nowEnd[0],
-//           userId: req.user.id,
-//         },
-//         {
-//           where: { userId: req.user.id },
-//         }
-//       );
-//     } else {
-//       await Cycle.update(
-//         {
-//           bleedEnd: nowEnd[0],
-//           userId: req.user.id,
-//         },
-//         {
-//           where: { userId: req.user.id },
-//         }
-//       );
-//     }
-//   }
-// };
+  console.log("요청 확인", req.body.action.parameters.DATE.value);
+  console.log(req.body.action.parameters);
+  // 생리주기 입력 정보를 유저가 확인하지 않았을 경우
+/*  try {
+    const exUser = await User.findOne({
+      attributes: ["id"],
+      where: { id: req.body.action.parameters.user_ID.value },
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+  if (!exUser) {
+    //아이디 틀린 경우
+    outputCycle = "아이디";
+    resObj.version = req.body.version;
+    resObj.output.outputCycle = outputCycle;
+    console.log(resObj);
+    res.json(resObj);
+    res.end();
+    return;
+  } else {
+    //아이디 틀리지 않은 경우
+    const exUser = await User.findOne({
+      attributes: ["id"],
+      where: { id: req.body.action.parameters.user_ID.value },
+    });
+    const nowStart = await Cycle.findAll({
+      attributes: ["bleedStart"],
+      where: {
+        [Op.and]: [
+          {bleedStart: {[Op.gte]: fin_start}},
+          {bleedStart: {[Op.lte]: fin_end}},
+          {userId: req.user.id},
+        ],
+      },
+      raw: true,
+    });
+    if(nowStart[0].bleedStart) {
+      await Cycle.create(
+          cycleStart: bleedStart
+    }
+  }
+
+
+    const date = req.body.action.parameters.DATE.value;
+    const exDate = await Date.findOne({
+      where: { date: date, userId: req.user.id },
+    });
+    if (exDate) {
+      res.send(exDate);
+    } else {
+      res.send("입력된 정보가 없습니다.");
+
+
+
+    // const nowStart = await Cycle.findAll({
+    //   limit: 1,
+    //   attributes: ["bleedStart"],
+    //   where: { userId: req.body.action.parameters.user_ID.value },
+    //   order: [["createdAt", "DESC"]],
+    //   raw: true,
+    // });
+    if (nowStart[0] === undefined) {
+      await Cycle.create(
+        {
+          bleedStart: nowStart[0],
+          userId: req.user.id,
+        },
+        {
+          where: { userId: req.user.id },
+        }
+      );
+    } else {
+      await Cycle.update(
+        {
+          bleedStart: nowCycle[0],
+          userId: req.user.id,
+        },
+        {
+          where: { userId: req.user.id },
+        }
+      );
+    }
+  }
 };
+
+const answerRealEnd = async (req, res, next) => {
+  console.log(req.body);
+  console.log(req.body.action.parameters);
+  // 생리주기 입력 정보를 유저가 확인하지 않았을 경우
+  try {
+    const exUser = await User.findOne({
+      attributes: ["id"],
+      where: { id: req.body.action.parameters.user_ID.value },
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+  if (!exUser) {
+    //아이디 틀린 경우
+    outputCycle = "아이디";
+    resObj.version = req.body.version;
+    resObj.output.outputCycle = outputCycle;
+    console.log(resObj);
+    res.json(resObj);
+    res.end();
+    return;
+  } else {
+    //아이디 틀리지 않은 경우
+    const nowEnd = await Cycle.findOne({
+      limit: 1,
+      attributes: ["bleedEnd"],
+      where: { userId: req.body.action.parameters.user_ID.value },
+      order: [["bleedEnd", "DESC"]],
+      raw: true,
+    });
+    if (nowEnd[0] === undefined) {
+      await Cycle.create(
+        {
+          bleedEnd: nowEnd[0],
+          userId: req.user.id,
+        },
+        {
+          where: { userId: req.user.id },
+        }
+      );
+    } else {
+      await Cycle.update(
+        {
+          bleedEnd: nowEnd[0],
+          userId: req.user.id,
+        },
+        {
+          where: { userId: req.user.id },
+        }
+      );
+    }
+  }*/
+};
+
 
 module.exports = {
   askMeanCycle,
